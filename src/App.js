d4d244f1443d2d134534a8b69095d94054431347
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-03-12 21:12:37 +0000

    add useLocation hook to catch url changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Line from "./Line";
 import { load, loadQuestions } from "./loading";
+import { useLocation } from "react-router-dom";
 import "./App.css";
 import sections from "../data/sections.json";
 import questions1 from "../data/4,5,6,7,8,11,12,15,16,17.json";
@@ -14,6 +15,11 @@ const App = () => {
   loadQuestions(questions2, QUESTIONS_INIT);
 
   const [questions, setQuestions] = useState(QUESTIONS_INIT);
+
+  const location = useLocation();
+  useEffect(() => {
+    console.log(location.hash);
+  }, [location]);
 
   const ROOT_LEVEL = 0;
   const POS_ON_ROOT_LEVEL = 0;
