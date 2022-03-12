e370eb5c515dc2e4b03bd8b1631eee385cb9fb67
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-03-12 14:42:25 +0000

    refactor functions from App.js to loading.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Line from "./Line";
+import { load, loadQuestions } from "./loading";
 import "./App.css";
 import sections from "../data/sections.json";
 import questions1 from "../data/4,5,6,7,8,11,12,15,16,17.json";
@@ -13,64 +14,6 @@ const App = () => {
   loadQuestions(questions2, QUESTIONS_INIT);
 
   const [questions, setQuestions] = useState(QUESTIONS_INIT);
-
-  function findChildren(json, parentId) {
-    const children = [];
-    json.forEach((element, index) => {
-      if (element.parentId === parentId) children.push(index);
-    });
-    return children;
-  }
-
-  //This function loads in the sections.json file. The data is structured in
-  //that array with key element pairs on a way, which is called a binary tree.
-  //The search operation requires a difficult algorithm in that data
-  //structure. We avoid that with mapping the data structure to a two-dimensional
-  //array. The first index means the level on the tree, the second index means
-  //the branch position on the given level of the tree. This way every search
-  //operation becomes elementary search in a 2D-array. This perhaps a relatively
-  //simple solution for this not so simple problem.
-  function load(json) {
-    const ROOT = -1;
-    const levels = [];
-    levels.push([
-      {
-        index: 0,
-        isOpen: false,
-        children: findChildren(json, ROOT),
-      },
-    ]);
-    let nextLevel, levelIndex;
-    levelIndex = 0;
-    do {
-      nextLevel = [];
-      const level = levels[levelIndex];
-      level.forEach((element) => {
-        element.children.forEach((index) => {
-          const parentId = json[index].id;
-          nextLevel.push({
-            index: index,
-            isOpen: false,
-            children: findChildren(json, parentId),
-          });
-        });
-      });
-      if (nextLevel.length > 0) levels.push(nextLevel);
-      levelIndex++;
-    } while (nextLevel.length > 0);
-    return levels;
-  }
-
-  function loadQuestions(questions, output) {
-    questions.forEach((question) => {
-      output.push({
-        question: question.question,
-        answer: question.answer,
-        showAnswer: false,
-        sectionId: question.sectionId,
-      });
-    });
-  }
 
   const ROOT_LEVEL = 0;
   const POS_ON_ROOT_LEVEL = 0;
